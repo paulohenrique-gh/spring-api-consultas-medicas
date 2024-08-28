973ab06ed6d04f3be0d309291128c2374978c677
Paulo Henrique <ph.1928@hotmail.com>
2024-08-28 10:11:07 -0300

    feat: User model and UserRole

---
+++ src/main/java/org/example/atividadespringsecurity/domain/user/User.java
@@ -0,0 +1,64 @@
+package org.example.atividadespringsecurity.domain.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String login;
+
+    private String password;
+
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
---
+++ src/main/java/org/example/atividadespringsecurity/domain/user/UserRole.java
@@ -0,0 +1,15 @@
+package org.example.atividadespringsecurity.domain.user;
+
+public enum UserRole {
+    ADMIN("admin"), DOCTOR("doctor"), PATIENT("patient"), RECEPTIONIST("receptionist");
+
+    private final String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    String getRole() {
+        return this.role;
+    }
+}
