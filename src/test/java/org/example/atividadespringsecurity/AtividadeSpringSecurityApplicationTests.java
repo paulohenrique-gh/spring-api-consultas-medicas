28c1dd296bd27570fa5321e04d7aa348c4c792b0
Paulo Henrique <ph.1928@hotmail.com>
2024-08-27 21:36:39 -0300

    First commit

---
+++ src/test/java/org/example/atividadespringsecurity/AtividadeSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.atividadespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
