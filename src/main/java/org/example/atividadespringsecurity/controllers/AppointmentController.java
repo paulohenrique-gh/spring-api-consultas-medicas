ec581a1a67d75d33e24746aaed11ad5d25e6c56d
Paulo Henrique <ph.1928@hotmail.com>
2024-08-28 15:25:59 -0300

    feat: return List of appointments by patient name
    Validates if user has only role PATIENT and returns only their appointments

--- src/main/java/org/example/atividadespringsecurity/controllers/AppointmentController.java
+++ src/main/java/org/example/atividadespringsecurity/controllers/AppointmentController.java
@@ -3,8 +3,13 @@ package org.example.atividadespringsecurity.controllers;
 import jakarta.validation.Valid;
 import org.example.atividadespringsecurity.domain.appointment.Appointment;
 import org.example.atividadespringsecurity.domain.appointment.AppointmentDTO;
+import org.example.atividadespringsecurity.domain.user.User;
 import org.example.atividadespringsecurity.services.AppointmentService;
+import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -23,6 +28,11 @@ public class AppointmentController {
 
     @GetMapping
     public ResponseEntity<List<Appointment>> getAllAppointments() {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        var authorities = user.getAuthorities();
+        if (authorities.size() == 1 && authorities.contains(new SimpleGrantedAuthority("ROLE_PATIENT"))) {
+           return ResponseEntity.ok(appointmentService.findByPatientName(user.getUsername()));
+        }
         return ResponseEntity.ok(appointmentService.findAll());
     }
 
@@ -40,6 +50,6 @@ public class AppointmentController {
                 .path("/{id}")
                 .buildAndExpand(savedAppointment.getId())
                 .toUri();
-        return ResponseEntity.created(location).build();
+        return ResponseEntity.created(location).body(savedAppointment);
     }
 }
