2e6c8e059a226c739bd1f6d00bce82cd56995818
Paulo Henrique <ph.1928@hotmail.com>
2024-08-28 12:06:23 -0300

    feat: create UserRepository

---
+++ src/main/java/org/example/atividadespringsecurity/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package org.example.atividadespringsecurity.repositories;
+
+import org.example.atividadespringsecurity.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByUsername(String username);
+}
