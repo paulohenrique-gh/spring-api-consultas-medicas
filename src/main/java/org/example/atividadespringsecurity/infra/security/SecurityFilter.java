f58567ec69f712b28bbf513aa3dfdb9e0d22b81f
Paulo Henrique <ph.1928@hotmail.com>
2024-08-28 22:05:22 -0300

    refactor: add token blacklist validation to SecurityFilter

--- src/main/java/org/example/atividadespringsecurity/infra/security/SecurityFilter.java
+++ src/main/java/org/example/atividadespringsecurity/infra/security/SecurityFilter.java
@@ -23,7 +23,13 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var token = this.recoverToken(request);
+        var token = this.tokenService.recoverTokenFromRequest(request);
+
+        if (this.tokenService.isBlacklisted(token)) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
         if (token != null) {
             var username = tokenService.validateToken(token);
             UserDetails user = userRepository.findByUsername(username);
@@ -35,9 +41,4 @@ public class SecurityFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    private String recoverToken(HttpServletRequest request) {
-        var authHeader = request.getHeader("Authorization");
-        if (authHeader == null) return null;
-        return authHeader.replace("Bearer ", "");
-    }
 }
