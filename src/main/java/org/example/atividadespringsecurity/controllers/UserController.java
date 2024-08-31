2fc5c643d46908b3c666e2984361907a8a9da649
Paulo Henrique <ph.1928@hotmail.com>
2024-08-31 09:52:30 -0300

    feat: add UserController with route users/my-info
    it returns UserInfoDTO with username and role

---
+++ src/main/java/org/example/atividadespringsecurity/controllers/UserController.java
@@ -0,0 +1,20 @@
+package org.example.atividadespringsecurity.controllers;
+
+import org.example.atividadespringsecurity.domain.user.User;
+import org.example.atividadespringsecurity.domain.user.UserInfoDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin
+public class UserController {
+
+    @GetMapping("/my-info")
+    public ResponseEntity<UserInfoDTO> getCurrentUserInfo() {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserInfoDTO userInfoDTO = new UserInfoDTO(user.getUsername(), user.getRole());
+        return ResponseEntity.ok(userInfoDTO);
+    }
+}
