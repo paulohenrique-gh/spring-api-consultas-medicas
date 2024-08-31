2a6ef815c3fecf8010db991a72637cc892c044bd
Paulo Henrique <ph.1928@hotmail.com>
2024-08-31 09:55:14 -0300

    refactor: change request mapping in UserController

--- src/main/java/org/example/atividadespringsecurity/controllers/UserController.java
+++ src/main/java/org/example/atividadespringsecurity/controllers/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 @CrossOrigin
 public class UserController {
 
